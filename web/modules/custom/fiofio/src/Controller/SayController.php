17ea1288e1dcf44a0ba06189e34d915201300d28
Matthieu Mota <matthieumota@gmail.com>
2023-08-08 16:04:11 +0200

    Dynamic route

--- web/modules/custom/fiofio/src/Controller/SayController.php
+++ web/modules/custom/fiofio/src/Controller/SayController.php
@@ -6,8 +6,8 @@ use Symfony\Component\HttpFoundation\Response;
 
 class SayController
 {
-    public function index()
+    public function index(int $count): Response
     {
-        return new Response('Salut, je suis Fiofio');
+        return new Response('Salut, je suis '.str_repeat('fio', max($count, 2)));
     }
 }
