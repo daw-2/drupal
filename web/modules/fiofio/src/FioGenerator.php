bafc6dc387b0fa3a7cd1811f243b0cf552b79a8d
Matthieu Mota <matthieumota@gmail.com>
2023-08-08 16:57:23 +0200

    Services in service

--- web/modules/fiofio/src/FioGenerator.php
+++ web/modules/fiofio/src/FioGenerator.php
@@ -2,10 +2,18 @@
 
 namespace Drupal\fiofio;
 
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+
 class FioGenerator
 {
+    public function __construct(private LoggerChannelFactoryInterface $logger)
+    {
+    }
+
     public function get(int $count): string
     {
+        $this->logger->get('default')->debug('test');
+
         return 'Salut, je suis '.str_repeat('fio', max($count, 2));
     }
 }
