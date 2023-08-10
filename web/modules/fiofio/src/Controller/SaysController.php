c8d7db513f5cd287cb0af5dbd45147212a0b269b
Matthieu Mota <matthieumota@gmail.com>
2023-08-10 15:11:36 +0200

    wip

--- web/modules/fiofio/src/Controller/SaysController.php
+++ web/modules/fiofio/src/Controller/SaysController.php
@@ -23,11 +23,17 @@ class SaysController extends ControllerBase
     public function index(int $count): array
     {
         $user = $this->currentUser();
+        // ou bien (à éviter en objet mais possible dans les hooks)
+        // \Drupal::service('current_user');
+        // \Drupal::currentUser();
+        // dump($user->getDisplayName());
 
         // return new Response($this->generator->get($count));
 
         return [
-            '#title' => $this->generator->get($count),
+            '#type' => 'markup',
+            '#markup' => $this->generator->get($count),
+            '#title' => 'Fiofio',
         ];
     }
 }
