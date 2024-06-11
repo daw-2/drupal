8c591445ec369a3da6be7478c5a89e9f57e791d7
Matthieu Mota <matthieumota@gmail.com>
2024-06-11 21:17:59 +0200

    Improve

--- web/themes/fiorella/js/app.js
+++ web/themes/fiorella/js/app.js
@@ -1,6 +1,18 @@
 (($, Drupal, once) => {
     $.fn.ajaxCallback = (email) => alert(email);
 
+    // DOM Chargé / Site classique
+    $(document).ready(() => {
+        $('article h2').click((e) => {
+            e.preventDefault();
+
+            $(element).prev().hide();
+            $(element).next().show();
+            $(element).hide();
+        });
+    });
+
+    // Changement d'état dans Drupal
     Drupal.behaviors.myFeature = {
         attach: function (context, settings) {
             once('myFeature', '.fio-truncate-btn', context).forEach(function (element) {
--- web/themes/fiorella/theme-settings.php
+++ web/themes/fiorella/theme-settings.php
@@ -22,4 +22,12 @@ function fiorella_form_system_theme_settings_alter(&$form, FormStateInterface $f
             '#default_value' => theme_get_setting('setting2'),
         ],
     ];
+
+    $form['#validate'][] = 'fiorella_validate_form';
+}
+
+function fiorella_validate_form(&$form, FormStateInterface $form_state) {
+    if ($form_state->getValue('setting1') === 'toto') {
+        $form_state->setErrorByName('setting1', 'Toto est une valeur interdite');
+    }
 }
